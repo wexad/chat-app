1d5b9db15b9d284c3fb521a3c02a717308bff19a
wexad <sherzodchoriyev747@gmail.com>
2024-04-09 08:26:19 +0500

    main

--- src/uz/pdp/ui/Main.java
+++ src/uz/pdp/ui/Main.java
@@ -38,6 +38,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome! To our new chat application! ");
+        boolean isFinished;
         while (true) {
             if (curUser == null) {
                 LoginView.start();
